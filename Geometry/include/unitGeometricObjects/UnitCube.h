35c10dd2b98f0a7b91d62690278bab462754dfe4
BerkayYildirimUA <Berkay.Yildirim@student.uantwerpen.be>
2024-10-15 10:32:47 +0200

    beginning cube

---
+++ Geometry/include/unitGeometricObjects/UnitCube.h
@@ -0,0 +1,17 @@
+//
+// Created by berka on 15/10/2024.
+//
+
+#ifndef RAYTRACINGENGINE_UNITCUBE_H
+#define RAYTRACINGENGINE_UNITCUBE_H
+
+#include "HitObject.h"
+
+class UnitCube : public HitObject {
+public:
+    bool hit(const Ray &incomingRay, Intersection &intersection) const override;
+
+};
+
+
+#endif //RAYTRACINGENGINE_UNITCUBE_H
---
+++ Geometry/src/unitGeometricObjects/UnitCube.cpp
@@ -0,0 +1,14 @@
+//
+// Created by berka on 15/10/2024.
+//
+
+#include "Geometry/include/unitGeometricObjects/UnitCube.h"
+
+bool UnitCube::hit(const Ray &incomingRay, Intersection &intersection) const {
+
+    Ray genRay = transformRayToObjectSpace(incomingRay);
+    double tHIt, numer, denom;
+    double tIn = -1000000.0, tOut = 1000000.0;
+    int inSurf, outSurf;
+
+}
