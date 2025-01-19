11dfbb3eacb13f950beb38537011e8fbec60f899
BerkayYildirimUA <Berkay.Yildirim@student.uantwerpen.be>
2025-01-19 03:23:54 +0100

    got rid of not needed files

--- cmake-build-release/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/UA/Semester7/ComputerGraphics/CppCode/RayTracingEngine
-# Build directory: D:/UA/Semester7/ComputerGraphics/CppCode/RayTracingEngine/cmake-build-release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("D:/UA/Semester7/ComputerGraphics/CppCode/RayTracingEngine/cmake-build-release/RayTracingEngineTests-b12d07c_include.cmake")
-subdirs("glfw_build")
